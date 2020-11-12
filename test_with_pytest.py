abe2e7d184858bbc78fd333b927a8f70cb2f519d
Schemtschik <schemtschik@gmail.com>
2020-11-12 12:40:17 +0300

    wip

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -50,3 +50,8 @@ def my_fixture():
 
 def test_with_fixture(my_fixture):
     assert my_fixture == 42
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 54)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
